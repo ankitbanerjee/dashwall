6c93c7a67d54fad62c152446b74254320ebc6cf5
ankitbanerjee <ankitbanerjee@rocketmail.com>
2025-04-29 12:41:23 +0530

    Update highlights.php
    Added the functionality where the Highlights section title is fetched from the field in the theme settings.
    Added the functionality to fetch page id and add the button pointing to that page if selected in the theme settings.
    Added the functionality where the button text is fetched from the field in the theme settings.

--- inc/highlights.php
+++ inc/highlights.php
@@ -12,18 +12,40 @@ $highlight = new WP_Query(
 );
 ?>
 
-<?php if ( ( get_theme_mod('highlight-posts-count','5') !='0') && $highlight->have_posts() ): ?>
+<?php if ( ( get_theme_mod('highlight-posts-count','5') != '0' ) && $highlight->have_posts() ) : ?>
 	<div class="page-card page-card-highlights stickywrap">
 		
-		<h3 class="stickywrap-heading"><?php esc_html_e('Highlights','dashwall'); ?></h3>
+		<h3 class="stickywrap-heading">
+			<?php 
+				// Get the section title from Customizer
+				$highlight_title = get_theme_mod( 'highlight-section-title', esc_html__( 'Highlights', 'dashwall' ) );
+				echo esc_html( $highlight_title );
+			?>
+		</h3>
 		
 		<div class="stickywrap-inner">
-			<?php while ( $highlight->have_posts() ): $highlight->the_post(); ?>
-				<?php get_template_part('content-highlights'); ?>
+			<?php while ( $highlight->have_posts() ) : $highlight->the_post(); ?>
+				<?php get_template_part( 'content-highlights' ); ?>
 			<?php endwhile; ?>
-		</div>	
-		
+		</div>
+
+		<?php
+			// Get the page ID selected in the customizer for highlights
+			$highlight_page_id = get_theme_mod( 'highlights-page', '' );
+			
+			// If a page is selected, display the button below the highlights
+			if ( $highlight_page_id ) :
+				$page_url = get_permalink( $highlight_page_id );
+				$button_text = get_theme_mod( 'highlight-button-text', 'View More Highlights' );
+		?>
+			<div class="highlight-button-container">
+				<a href="<?php echo esc_url( $page_url ); ?>" class="highlight-button">
+					<?php echo esc_html( $button_text ); ?>
+				</a>
+			</div>
+		<?php endif; ?>
+
 	</div>
 <?php endif; ?>
-	
+
 <?php wp_reset_postdata(); ?>
